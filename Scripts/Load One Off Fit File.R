c4b89e3e128f7c1cda5a311d332c5d725bb1c735
forem056 <forem056@umn.edu>
2020-11-26 20:51:10 -0600

    Garmin HRV and Big Data Analysis
    Unsuccessfully tried to reproduce Big Data Running Performance analysis. Also got HRV data from a small Garmin file and tried to read it into R using the RHRV package. This code is stored under the Load One Off Fit File

--- Scripts/Load One Off Fit File.R
+++ Scripts/Load One Off Fit File.R
@@ -1,24 +1,33 @@
 #Load one file
 
 library(fitFileR); library(data.table); library(tidyverse)
-loadfile <- "C:\\Users\\Nick.Foreman\\Desktop\\Nick Mobile Folders\\School\\2020-2021\\R Files\\RandomFitFiles\\A6NI5029.fit"
+loadfile <- "C:\\Users\\Nick.Foreman\\Desktop\\Nick Mobile Folders\\School\\Personal Running\\enable_hrv_settings_file.fit"
 GarminFile <- readFitFile(loadfile)
 
-loadfile2 <- "C:\\Users\\Nick.Foreman\\Desktop\\Nick Mobile Folders\\School\\2020-2021\\R Files\\OldFitFiles\\5415270890.fit"
+loadfile2 <- "HRVtest.FIT"
 GarminFile2 <- readFitFile(loadfile2)
 
-loadfile3 <- "C:\\Users\\Nick.Foreman\\Desktop\\Nick Mobile Folders\\School\\2020-2021\\R Files\\RandomFitFiles\\LastFile4.fit"
-GarminFile3 <- readFitFile(loadfile3)
+GarminFile.lapp <- lapply(GarminFile2, data.frame, stringsAsFactors = FALSE)
+GarminFile <- lapply(GarminFile, data.frame, stringsAsFactors = FALSE)
 
-GarminFile.asDF <- as.data.frame(GarminFile2) #DOESN'T WORK
-
-GarminFile.DF <- data.frame(GarminFile2) #DOESN'T WORK
-
-GarminFile.rbind <- rbindlist(GarminFile2, fill = TRUE) #DOESN'T WORK
-
-GarminFile.lapp <- lapply(GarminFile2, data.frame, stringsAsFactors = FALSE) #WORKS
-
-class(GarminFile.asDF)
-class(GarminFile.DF)
-class(GarminFile.rbind)
-class(GarminFile.lapp)
+library(purrr)
+as.numeric(map(GarminFile2$hrv$time[1],1)) #pulls out RR interval from hrv
+length(GarminFile2$record$heart_rate) == length(GarminFile2$hrv$time) #length of the files is the same!
+rr <- matrix(data = NA, nrow = 13, ncol = 1) #creates matrix to store data
+for (i in 1:13){
+  rr[i,] <- as.numeric(map(GarminFile2$hrv$time[i],1))
+  
+} #pulls RR data out and adds to matrix
+colnames(rr)[1] <- "RR" #sets column name to RR
+write.csv(rr, file = "QuickHRVTest.csv", row.names = F) #writes to csv, doesn't seem to help with reading in though
+write.table(rr, file = "QuickHRVTest.txt", row.names = F, col.names = F) #writes to txt
+library(RHRV) #load HRV library 
+hrv.data <- CreateHRVData() #create structure to store HRV data
+hrv.data <- SetVerbose(hrv.data, T)
+#hrv.data <- LoadBeatRR(hrv.data, RecordName = "QuickHRVTest.csv", RecordPath = ".", scale = 0.001)
+  #Doesn't work, not sure why
+hrv.data <- LoadBeat(fileType = "RR", HRVData = hrv.data, Recordname = "QuickHRVTest.txt")
+nihr <- BuildNIHR(hrv.data)
+nihr$Beat$RR; rr #comparing RR intervals
+nihr$Beat$niHR; GarminFile2$record$heart_rate
+PlotNIHR(nihr)
