866226d22f45d15f7668c602f3f848460e741be9
forem056 <forem056@umn.edu>
2020-12-11 09:02:42 -0600

    Examine HRV from a workout
    Looking at RR intervals in a workout as opposed to just a stationary 10 second window. Still need to troubleshoot difference between HR and RR intervals

--- Scripts/Load One Off Fit File.R
+++ Scripts/Load One Off Fit File.R
@@ -1,14 +1,14 @@
 #Load one file
 
 library(fitFileR); library(data.table); library(tidyverse)
-loadfile <- "C:\\Users\\Nick.Foreman\\Desktop\\Nick Mobile Folders\\School\\Personal Running\\enable_hrv_settings_file.fit"
+loadfile <- "E:\\GARMIN\\ACTIVITY\\AC8F0553.FIT" #load file straight from the watch
 GarminFile <- readFitFile(loadfile)
 
 loadfile2 <- "HRVtest.FIT"
 GarminFile2 <- readFitFile(loadfile2)
 
-GarminFile.lapp <- lapply(GarminFile2, data.frame, stringsAsFactors = FALSE)
-GarminFile <- lapply(GarminFile, data.frame, stringsAsFactors = FALSE)
+#GarminFile.lapp <- lapply(GarminFile2, data.frame, stringsAsFactors = FALSE)
+#GarminFile <- lapply(GarminFile, data.frame, stringsAsFactors = FALSE)
 
 library(purrr)
 as.numeric(map(GarminFile2$hrv$time[1],1)) #pulls out RR interval from hrv
@@ -31,3 +31,41 @@ nihr <- BuildNIHR(hrv.data)
 nihr$Beat$RR; rr #comparing RR intervals
 nihr$Beat$niHR; GarminFile2$record$heart_rate
 PlotNIHR(nihr)
+
+##### Looking at HRV in workout #####
+library(purrr)
+as.numeric(map(GarminFile$hrv$time[1],1)) #pulls out RR interval from hrv
+length(GarminFile$record$heart_rate) == length(GarminFile$hrv$time) #length of the files is not the same, sadly
+record <- GarminFile$record
+plot(record$timestamp, record$heart_rate) #shows HR vs. time
+#record$timestamp[which(record$heart_rate < 90)] #gives timestamps when HRM disconnected
+  #Looks like right around 15:25 and 15:35
+#head(record$timestamp)
+rr <- matrix(data = NA, nrow = length(GarminFile$hrv$time), ncol = 1) #creates matrix to store data
+for (i in 1:length(GarminFile$hrv$time)){
+  rr[i,] <- as.numeric(map(GarminFile$hrv$time[i],1))
+  
+} #pulls RR data out and adds to matrix
+write.table(rr, file = "LongRun.HRVTest.txt", row.names = F, col.names = F) #writes to txt
+
+library(RHRV) #load HRV library 
+hrv.data <- CreateHRVData() #create structure to store HRV data
+hrv.data <- SetVerbose(hrv.data, T)
+hrv.data <- LoadBeat(fileType = "RR", HRVData = hrv.data, Recordname = "Workout4x7T.HRVTest.txt")
+nihr <- BuildNIHR(hrv.data)
+class(rr)
+rrcomp <- data.frame(nihr$Beat$RR[2:length(nihr$Beat$RR)], rr) #comparing RR intervals
+colnames(rrcomp)[1] <- "nihr.RR"
+rrcomp$nihr.RR.scaled <- rrcomp$nihr.RR/1000
+rrcomp$difference <- rrcomp$nihr.RR.scaled - rrcomp$rr
+plot(record$heart_rate)
+length(nihr$Beat$niHR)
+plot(record$heart_rate[1:3087], nihr$Beat$niHR[1:3087])
+
+#no difference between nihr RR and measured RR even without filtering
+rrcomp %>% arrange(desc(abs(difference)))  %>% head(n=5) 
+
+nihr.filt <- RHRV::FilterNIHR(nihr, minbpm = 120, maxbpm = 180)
+plot(record$heart_rate[1:2204], nihr.filt$Beat$niHR)
+plot(nihr.filt$Beat$Time, nihr.filt$Beat$niHR) #plots filtered HR vs time, appears similar to Garmin HR graph
+
