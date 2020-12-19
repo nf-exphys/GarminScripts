6f37b4f4dd52075c3cc8893ebcdb364b418d83a3
forem056 <forem056@umn.edu>
2020-12-18 22:15:29 -0600

    Create DataCleaning.R
    Loaded all data through 12-17-2020, removed duplicates, and got rid of all non-running data

---
+++ Scripts/DataCleaning.R
@@ -0,0 +1,105 @@
+library(tidyverse, quietly = T)
+##### Load 2020 Data #####
+  #Long term, I think calling this data from SQL is probably the way to go
+  #So much of what I'm doing is dependent on relational data
+load(file = "./Objects/FitDFCleanSort2020-10-12.Rdata") #loads data
+Fit.DF2 <- Fit.DF #renames to avoid being overwritten
+
+load(file = "./Objects/FitDFCleanSort2020-08-24.Rdata") #loads data
+data <- c(Fit.DF, Fit.DF2) #combines first two data loads
+remove(Fit.DF, Fit.DF2) #to avoid confusion
+
+load(file = "./Objects/FitDFCleanSort2020-12-18_Oct11_Oct31.Rdata") #loads data
+Fit.DF3 <- Fit.DF #renames to avoid being overwritten
+
+load(file = "./Objects/FitDFCleanSort2020-12-18_Oct31_Dec17.Rdata") #loads data
+Fit.DF4 <- Fit.DF #renames for clarity
+
+data <- c(data, Fit.DF3, Fit.DF4)
+remove(Fit.DF, Fit.DF3, Fit.DF4)
+
+##### Checking for Duplicates & Sequential Ordering #####
+n <- length(data)
+repeat_check <- tibble()
+for (i in 1:n){
+  repeat_check[i,1] <- as.POSIXct(data[[i]]$sum_data$start_time, origin = "1970-01-01")
+  repeat_check[i,2] <- as.numeric(i)
+}
+colnames(repeat_check) <- c("start", "num")
+
+setdiff(which(duplicated(repeat_check$start) == T), which(repeat_check$start=="1969-12-31 17:59:59"))
+  #Compares duplicates in repeat_check to lines in repeat_check from manually created activities, which have date of 1969
+  #If setdiff returns nothing, we know all of the repeats are due to manually created activities
+  #If setdiff returns something, such as 1351, run this to remove it: data[1351] <- NULL
+
+#Checking to make sure every item is uploaded in sequential order
+repeat_check$progress <- as.logical(factor(ifelse(repeat_check$start > lag(repeat_check$start),'T','F')))
+repeat_check %>% #outputs any files out of order
+  filter(progress == F & start > "2020-07-01") #Checked and 1407 is an indoor ride, don't care *shrug*
+
+#Discard data from before July 2020 due to high frequency of indoor running, possibly inaccurate HR data, etc. 
+  #Other data is still worth keeping, just not for modeling, at least not right now
+  #Eventually, might be worth going back through a couple of seasons with some racing to strengthen modeling
+start_date <- repeat_check %>% filter(start > "2020-07-01") %>% head(1) %>% pull(num)
+data[1:(start_date-1)] <- NULL
+remove(repeat_check)
+
+##### Keeping Only Running Data #####
+
+#Initial screening
+  #Printing out data items that don't match or that have HRV data
+  #Ultimately removing names of data 
+for (i in 1:length(data)){
+  goal <- names(pluck(data, 1)) #standard for order of names
+  check <- names(pluck(data, i)) #check for each one
+  
+  if (("hrv" %in% check) == T){ #check for HRV data
+    print(paste0("hrv_", i))
+  } else {ifelse(all(diff(match(goal, check)) > 0)==F, print(i), next)} 
+  #if no HRV data, compare to goal and print i if it doesn't match
+}
+
+data[84:85] <- NULL #removed based on printed i above
+  #Seems like the problem is Connect-created files
+
+#Use sum_data to identify events that aren't running
+#Somehow, the alphabetical sorting of names in sum_data didn't quite work for all of the data 
+for (i in 1:length(data)){
+  data[[i]]$sum_data <- data %>% 
+    pluck(.,i,"sum_data") %>% #selecting sum_data for each item in data
+    select(sort(names(.))) #sorting names alphabetically
+    #Then re-writes sum data
+}
+
+sum_data <- data %>% #combine sum data into one big data frame
+  map(., "sum_data") %>%
+  bind_rows(.id = "data_index")
+
+unique(sum_data$name) #shows options for name
+
+not_running_in_data <- sum_data %>% #find events where the name isn't Run
+  filter(name != "Run") %>%
+  pull(data_index) %>%
+  as.numeric()
+
+length(data) #length before removing
+data[c(not_running_in_data)] <- NULL
+length(data) #length after removing
+
+#Creating data frames for each type of data, with data_index as the # of each from data
+  #If this is the way I'm going to go, it might be easier to just use all of the CSVs
+event <- data %>% map(.,"event") %>%
+  bind_rows(.id = "data_index")
+
+lap <- data %>% map(.,"lap") %>%
+  bind_rows(.id = "data_index")
+
+device_info <- data %>% 
+  map(.,"device_info") %>%
+  bind_rows(.id = "data_index")
+
+record <- data %>% map(.,"record") %>%
+  bind_rows(.id = "data_index")
+
+#Not ready for HRV data yet - need to figure out how to handle/process it first
+
