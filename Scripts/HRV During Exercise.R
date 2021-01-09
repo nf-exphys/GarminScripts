b0c2a21c3237ee9a6390fbbf4562a58754ae0720
nf-exphys <68116178+nf-exphys@users.noreply.github.com>
2021-01-09 13:21:46 -0600

    Easy long run HRV
    Spent more time looking at HRV during exercise, saved couple images from easy long run

--- Scripts/HRV During Exercise.R
+++ Scripts/HRV During Exercise.R
@@ -12,29 +12,42 @@ hrv <- prog_bike$hrv$time #set the hrv data as its own object for more succinct 
 hrv <- long_run$hrv$time
 hrv <- easy_run$hrv$time
 
+#Alternatively, can read in Exercise HRV data from CSV
+  #Example: CSV from 01/09/2021 easy long run with Dylan
+hrv <- read_csv(file = "./Data/ExportedRunData/Cleaned_CSVs/2021_01_09 10_37_24 hrvdata.csv")
+
 rr_data <- matrix(ncol=1)
 #Pulls together RR intervals where RR is less than 1 (which it should be for this ride based on collab notebook)
 for (i in 1:length(hrv)){
-  if (i==1){
-    rr_data <- tibble(hrv=as.numeric(unlist(hrv[i]))) %>%
-      pivot_longer(cols = hrv, names_to = NULL, values_to = "rr") %>%
-      mutate(rr = case_when(rr < 1 ~ rr)) #drops values greater than 1
-  } else{
-    rr_data <- tibble(hrv=as.numeric(unlist(hrv[i]))) %>%
-      pivot_longer(cols = hrv, names_to = NULL, values_to = "rr") %>%
-      mutate(rr = case_when(rr < 1 ~ rr)) %>% #drops values greater than 1
-      bind_rows(rr_data,.) %>%
-      filter(is.na(rr) == F) %>%
-      select(rr)
+  if("list" %in% class(hrv)){
+    if (i==1){
+      rr_data <- tibble(hrv=as.numeric(unlist(hrv[i]))) %>%
+        pivot_longer(cols = hrv, names_to = NULL, values_to = "rr") %>%
+        mutate(rr = case_when(rr < 1 ~ rr)) #drops values greater than 1
+      } else{
+      rr_data <- tibble(hrv=as.numeric(unlist(hrv[i]))) %>%
+        pivot_longer(cols = hrv, names_to = NULL, values_to = "rr") %>%
+        mutate(rr = case_when(rr < 1 ~ rr)) %>% #drops values greater than 1
+        bind_rows(rr_data,.) %>%
+        filter(is.na(rr) == F) %>%
+        select(rr)
+    }
+  } else {
+    stop("HRV data isn't a list. This for loop isn't needed")
   }
+  
 }
-nrow(rr_data)
+
+rr_data <- hrv %>% 
+  rename(rr = hrv) %>%
+  mutate(rr = case_when(rr < 60 ~ rr)) %>%
+  drop_na()
 
 #Plot of RR intervals over time
 ggplot(data = rr_data, aes(y=rr, x=row_number(rr))) + geom_point()
 
 #Now need to do artifact correction
-artifact_correction_threshold <- 0.10
+artifact_correction_threshold <- 0.05
 i=2
 filtered_rr_data <- list()
 
@@ -55,15 +68,16 @@ plot(unlist(filtered_rr_data))
 
 #Adjust filter based on plot
 filtered_rr_data <- filtered_rr_data %>% 
-  unlist %>% as_tibble_col()
-  #filter(value < 0.65) 
+  unlist %>% as_tibble_col() %>%
+  filter(value < 0.7) 
 
-write.table(filtered_rr_data, file = "./Text Files/01_02_2021_progression_ride_impute.txt", row.names = F, col.names = F) #writes to txt
+#writes to txt
+write.table(filtered_rr_data, file = "./Text Files/01_09_2021_easy_long_run.txt", row.names = F, col.names = F) 
 
 hrv.data <- CreateHRVData() #create structure to store HRV data
 hrv.data <- SetVerbose(hrv.data, T)
 hrv.data <- LoadBeat(fileType = "RR", HRVData = hrv.data, 
-                     Recordname = "./Text Files/01_02_2021_progression_ride_impute.txt")
+                     Recordname = "./Text Files/01_09_2021_easy_long_run.txt")
 nihr <- BuildNIHR(hrv.data)
 PlotNIHR(nihr) #close but not quite
 
@@ -72,7 +86,9 @@ PlotNIHR(nihr) #better
 
 #Split data and find DFA
 library(nonlinearTseries)
-step <- 180 #number of seconds per window
+step <- 300 #number of seconds per window
+
+#Number of minutes divided by window size
 n<- max(nihr$Beat$Time)/step
 
 nihr$Beat$split <- cut_interval(1:nrow(nihr$Beat), floor(n), boundary = 0)
@@ -109,7 +125,7 @@ for (i in 1:length(split_data)){
 
 dfa_analysis <- nonlinearTseries::dfa(time.series = split_data[[i]]$RR, npoints = 30, window.size.range = c(4,300))
 
-regression_range <- c(4,30) #based on Kubios
+regression_range <- c(4,40) #based on Kubios
 
 dfa_est <- estimate(dfa_analysis, do.plot=T, regression.range = regression_range)
 dfa_est[1]
@@ -119,5 +135,5 @@ dfa_list <- rlist::list.append(dfa_list, dfa_est[1])
 summary_dfa <- dfa_list %>% unlist() %>% as_tibble_col() %>% cbind(summary_dfa,.)
 
 summary_dfa %>%
-#  filter(value < 0.75) %>%
-ggplot(data = ., aes(x=HR, y=value)) + geom_line()
+  #filter(value < 0.75) %>%
+ggplot(data = ., aes(x=HR, y=value)) + geom_point()
