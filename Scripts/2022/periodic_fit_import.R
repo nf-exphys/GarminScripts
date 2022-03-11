8299ac4058e97b874fca8e4427ea983686ff2155
forem056 <nick.foreman@pillarit.com>
2022-03-11 15:19:05 -0600

    Switch from for loop to custom function with lapply
    Also remove zip file comparison/extraction since that's now in the connect_scraping python script

--- Scripts/2022/periodic_fit_import.R
+++ Scripts/2022/periodic_fit_import.R
@@ -1,34 +1,15 @@
 
 source("./Scripts/2022/master_fit_func.R")
 
-#### Automate Transfer/Extraction of Zip Files ####
-download_location <- "./Data/zip_fit_files/"
-storage_location <- "./Data/raw_fit_files"
+download_location <- "./Data/zip_fit_files/" #location for zip download
+storage_location <- "./Data/raw_fit_files" #location for storage of fit files
 
-#this is going to break
-fit_already_done <- read_csv("./Data/fit_files_already_extracted.csv")
+#Give warning if zip files haven't been extracted/copied
+if (length(list.files(download_location) != 0)){
+  
+  warning("Not all zip files have been extracted")
 
-# storage_location <- "./Data/processed_fit_files" #fit files go here once analyzed
-
-zip_files <- list.files(path = download_location, 
-                        pattern = "*.zip", full.names = TRUE)
-
-lapply(zip_files, unzip, exdir = storage_location)
-
-#Make a data frame with names of all the fit files that have been extracted
-file_names <- as_tibble(zip_files) %>%
-  rename(fit_name = value) %>%
-  mutate(fit_name = str_remove(fit_name, download_location),
-         fit_name = str_remove(fit_name, ".zip"))
-
-#add files that were just unzipped to end of CSV
-file_names <- rbind(fit_already_done, file_names)
-
-#Write this to a file to compare for future imports
-write_csv(file_names, file = "./Data/fit_files_already_extracted.csv")
-
-#Now that zip files have been extracted, they can be deleted
-file.remove(zip_files)
+}
 
 #### Find New Fit Files ####
 
@@ -43,8 +24,10 @@ fit_to_read <- as_tibble(fit_files) %>%
          fit_name = str_remove(fit_name, "_ACTIVITY.fit"),
          fit_name = as.numeric(fit_name))
 
+fit_already_done <- read_csv("./Data/fit_files_already_extracted.csv") 
+
 #list of file numbers that could be read in but haven't been yet
-fit_to_read <- setdiff(c(fit_to_read), c(fit_already_done))
+fit_to_read <- setdiff(fit_to_read$fit_name, fit_already_done$fit_name)
 
 #Add file path for matching to fit_files
 fit_to_read <- paste0(storage_location, "/", fit_to_read, "_ACTIVITY.fit")
@@ -66,44 +49,67 @@ all_data <- lapply(fit_files, readFitFile)
 
 data_to_write <- list("file_info", "record", "lap", "event", "hrv", "device_info")
 
-for (i in 1:length(all_data)){
+#this seems to work, not sure why I wasn't using it before?
+lapply(fit_files, process_fit_data)
+
+#now check the fit files in file_info folder 
+fit_files <- list.files("./Data/processed_fit/file_info", full.names = TRUE)
+for (i in 1:length(fit_files)){
   
-  fit_id <- str_match_all(string = fit_files[[i]], 
-                          pattern = "[0-9]+") %>% 
-    unlist() %>% 
-    as.numeric()
-  
-  #get data
-  file_info <- get_data("file_id")
-  record <- get_data("record")
-  lap <- get_data("lap")
-  event <- get_data("event")
-  hrv <- get_data("hrv")
-  device_info <- get_data("device_info")
-  
-  #get the year that the file was made
-  year <- str_extract(string = as.character(file_info$time_created), pattern = "[:digit:]{4}")
-  year <- as.numeric(year)
-  
-  #don't read in data older than 2020
-  if(year < 2020){
-    print(paste0("file #", i, " was made in ", year, "and isn't newer than 2020. Skipping."))
+  csv <- read_csv(fit_files[i], col_select = "key", show_col_types = FALSE)
+  if (unique(csv$key) %in% fit_already_done$fit_name){
     next
+  } else{
+    key = as_tibble(unique(csv$key))
+    names(key)[1] = "fit_name"
+    fit_already_done <- bind_rows(fit_already_done, key)
   }
   
-  #merge sport and file_info because they're both always one row
-  file_info <- bind_cols(file_info, sport, .name_repair = "unique")
-  
-  #determine whether HRM was connected
-  file_info$hrm_connect <- hrm_connected(device_info)
-  
-  time_created <- as.character(file_info$time_created)
-  time_created <- str_replace_all(string = time_created, pattern = "[\\s|:|-]", replacement = "_")
-  #convert POSIXct to character and replace special characters
-  
-  sink(nullfile())
-  lapply(data_to_write, write_csv_path)
-  sink()
+  if (i == length(fit_files)){
+    write_csv(fit_already_done, "./Data/fit_files_already_extracted.csv")    
+  }
   
 }
 
+#Old for loop to process data
+
+# for (i in 1:length(all_data)){
+
+# fit_id <- str_match_all(string = fit_files[[i]], 
+#                         pattern = "[0-9]+") %>% 
+#   unlist() %>% 
+#   as.numeric()
+# 
+# #get data
+# file_info <- get_data("file_id")
+# record <- get_data("record")
+# lap <- get_data("lap")
+# event <- get_data("event")
+# hrv <- get_data("hrv")
+# device_info <- get_data("device_info")
+# 
+# #get the year that the file was made
+# year <- str_extract(string = as.character(file_info$time_created), pattern = "[:digit:]{4}")
+# year <- as.numeric(year)
+# 
+# #don't read in data older than 2020
+# if(year < 2020){
+#   print(paste0("file #", i, " was made in ", year, "and isn't newer than 2020. Skipping."))
+#   next
+# }
+# 
+# #merge sport and file_info because they're both always one row
+# file_info <- bind_cols(file_info, sport, .name_repair = "unique")
+# 
+# #determine whether HRM was connected
+# file_info$hrm_connect <- hrm_connected(device_info)
+# 
+# time_created <- as.character(file_info$time_created)
+# time_created <- str_replace_all(string = time_created, pattern = "[\\s|:|-]", replacement = "_")
+# #convert POSIXct to character and replace special characters
+# 
+# sink(nullfile())
+# lapply(data_to_write, write_csv_path)
+# sink()
+# 
+# }
