494add2b9ca65f31ea95af108f836ad8f95a2363
forem056 <nick.foreman@pillarit.com>
2023-01-10 12:25:30 -0500

    Update schedule_garmin_download.bat
    Update batch file with comments and code to run R script too

--- schedule_garmin_download.bat
+++ schedule_garmin_download.bat
@@ -1,2 +1,16 @@
-python %~dp0\connect_scraping.py
-pause
+set root=C:\Users\Nick.Foreman\Anaconda3
+
+call %root%\Scripts\activate.bat %root% 
+
+%root%\python.exe "%~dp0\connect_scraping.py"
+
+TIMEOUT /T 183
+
+"C:\Program Files\R\R-4.1.3\bin\R.exe" CMD BATCH "%~dp0\Scripts\current\periodic_fit_import.R"
+
+TIMEOUT /T 10
+
+REM root is the location for anaconda, and the activate.bat is used to open Anaconda
+REM this bypasses problems with module loading & modules being in the right spot
+REM %~dp0 fills in the path with the current file location
+REM timeout adds a delay before running periodic_fit_import to clean the files
